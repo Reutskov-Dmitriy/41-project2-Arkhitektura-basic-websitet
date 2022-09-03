358a6d485cfd61db1ea9c0d190602de722cd58db
Reutskov-Dmitriy <infowelc@gmail.com>
2022-09-03 23:37:18 +0300

    add section news html, css

--- js/script.js
+++ js/script.js
@@ -1,6 +1,12 @@
 $(document).ready(function () {
 	$('.slider').slick({
 		dots: true,
+		arrows: false,
+		speed: 2000,
+		autoplay: 4000,
+		pauseonHover: true,
+
 		// variableWidth: true,
 	});
+
 });
